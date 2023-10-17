af8896a0e04cb750532c442403454e20812fa860
Alexis-NM <alexis.na.me@gmail.com>
2023-10-17 13:36:58 +0200

    Matching function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-{"src" : "../public/img/6.png"},
-{"src" : "../public/img/38.png"},
-{"src" : "../public/img/55.png"},
-{"src" : "../public/img/65.png"},
-{"src" : "../public/img/94.png"},
-{"src" : "../public/img/106.png"},
-{"src" : "../public/img/131.png"},
-{"src" : "../public/img/143.png"},
+{"src" : "../public/img/6.png", matched : false},
+{"src" : "../public/img/38.png", matched : false},
+{"src" : "../public/img/55.png", matched : false},
+{"src" : "../public/img/65.png", matched : false},
+{"src" : "../public/img/94.png", matched : false},
+{"src" : "../public/img/106.png", matched : false},
+{"src" : "../public/img/131.png", matched : false},
+{"src" : "../public/img/143.png", matched : false},
 ]
 
 function App() {
@@ -37,11 +37,33 @@ const [choiseTwo, setChoiceTwo] = useState(null)
 
 const handleChoice = (card) => {
   choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+
 }
+
+//compare 2 selected card
+
+useEffect(()=> {
+  if (choiceOne && choiceTwo) {
+    if (choiseOne.src === choiceTwo.src) {
+      setCards(prevCards => {
+        return prevCards.map(card => {
+          if (card.src === choiceOne.src) {
+            return {...card, matched: true}
+          } else {
+            return card
+          }
+        })
+      })
+      resetTurn()
+    }else {
+      resetTurn()
+    }
+  }
+}, [choiceOne, choiceTwo])
 
 // reset choices & increase turn
 
-const resetTiurn = () => {
+const resetTurn = () => {
   setChoiceOne(null)
   setChoiceTwo(null)
   setTurns(prevTurns => prevTurns + 1)
