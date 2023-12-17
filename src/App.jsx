589b21d261620ca9cd967caa041e6249da5f77e7
Alexis-NM <alexis.na.me@gmail.com>
2023-12-17 13:37:26 +0100

    final touches added

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,95 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import SingleCard from './components/SingleCard'
+import { useEffect, useState } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-{"src" : "img/1.jpg", matched : false },
-{"src" : "img/2.jpg", matched : false },
-{"src" : "img/3.jpg", matched : false },
-{"src" : "img/4.jpg", matched : false },
-{"src" : "img/5.jpg", matched : false },
-{"src" : "img/6.jpg", matched : false },
-{"src" : "img/7.jpg", matched : false },
-{"src" : "img/8.jpg", matched : false },
-]
+  { src: "img/1.jpg", matched: false },
+  { src: "img/2.jpg", matched: false },
+  { src: "img/3.jpg", matched: false },
+  { src: "img/4.jpg", matched: false },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
-const [cards, setCards] = useState([])
-const [turns, setTurns] = useState(0)
-const [choiceOne, setChoiceOne] = useState(null)
-const [choiceTwo, setChoiceTwo] = useState(null)
-
-
-  // shuffle cards
+  // Carte aléatoire
 
   const shuffleCards = () => {
-    
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({...card, id : Math.random() }))
-  
-    setCards(shuffledCards)   
-    setTurns(0)
+      .map((card) => ({ ...card, id: Math.random() }));
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  //Faire un choix
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  //Comparer les 2 cartes selectionnées
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
     }
+  }, [choiceOne, choiceTwo]);
 
-//handle a choice
+  // Remettre à zero le choix
 
-const handleChoice = (card) => {
-  choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
 
-}
+  //Démarer un nouveau jeu automatiquement
 
-//compare 2 selected card
-
-useEffect(()=> {
-  if (choiceOne && choiceTwo) {
-
-    if (choiceOne.src === choiceTwo.src) {
-      setCards(prevCards => {
-        return prevCards.map(card => {
-          if (card.src === choiceOne.src) {
-            return {...card, matched: true}
-          } else {
-            return card
-          }
-        })
-      })
-      resetTurn()
-    }else {
-      setTimeout(() => resetTurn(), 1000)
-  }
-}
-}, [choiceOne, choiceTwo])
-
-// reset choices & increase turn
-
-const resetTurn = () => {
-  setChoiceOne(null)
-  setChoiceTwo(null)
-  setTurns(prevTurns => prevTurns + 1)
-}
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button onClick ={shuffleCards}>New Game</button>
-     
-      <div className= "card-grid">
-      {cards.map(card => (
-        <SingleCard 
-            key={card.id} 
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
-        />
+            disabled={disabled}
+          />
         ))}
       </div>
+      <p>Turns : {turns}</p>
     </div>
   );
 }
 
-export default App
+export default App;
